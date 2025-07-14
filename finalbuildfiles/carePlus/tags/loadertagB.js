c6d324e8a795d0a425bdfa1eedbbc63d0648c166
snktcodes <snktcodes@gmail.com>
2025-07-14 19:14:38 +0530

    committt

---
+++ finalbuildfiles/carePlus/tags/loadertagB.js
@@ -0,0 +1,3 @@
+document.write('<iframe src="https://tech-iion.github.io/advertiser-creatives/finalbuildfiles/carePlus/B/index.html" ' +
+    'width="320px" height="480px" frameborder="0" scrolling="no" ' +
+    'allow="autoplay; fullscreen; encrypted-media;"></iframe>');
