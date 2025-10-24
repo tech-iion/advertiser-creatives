cf0b5c8ee012ee824d6dcc4a8f48554376af63d2
snktcodes <snktcodes@gmail.com>
2025-10-24 20:50:42 +0530

    committtt

---
+++ finalbuildfiles/2025/Oct/doejeDutchGovt/tags/loadertagA.js
@@ -0,0 +1,3 @@
+document.write('<iframe src="https://tech-iion.github.io/advertiser-creatives/finalbuildfiles/2025/Oct/doejeDutchGovt/tags/playable/index.html?clickTAG=%%c1;cpdir=https://veiliginternetten.nl/doe-je-updates/" ' +
+    'width="320px" height="480px" frameborder="0" scrolling="no" ' +
+    'allow="autoplay; fullscreen; encrypted-media;"></iframe>');
